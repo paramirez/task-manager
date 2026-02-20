8b435ce17141902bd2aa13830516134bfc7b1c64
Pablo Ramírez <pabloandres2700@gmail.com>
2026-02-19 23:05:56 -0500

    chore: remove unused notification noop publisher and pg dependency

--- src/modules/notification/infrastructure/providers/NoopTaskEventPublisher.ts
+++
@@ -1,8 +0,0 @@
-import { TaskEventPublisher } from '@/modules/notification/application/ports/TaskEventPublisher';
-import { PromiseResult, Result } from '@/shared/core/result';
-
-export class NoopTaskEventPublisher implements TaskEventPublisher {
-  publish(): PromiseResult<void, Error> {
-    return Promise.resolve(Result.ok(undefined));
-  }
-}
